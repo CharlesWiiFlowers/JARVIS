47e869b5198f4a42063bd816d38c075e033f11ac
charleswiiflowers <carlitosfg.5051@gmail.com>
2026-07-02 22:48:51 -0600

    [🐍] Feature: Created Event BUS

--- core/event_bus.py
+++ core/event_bus.py
@@ -1,6 +1,26 @@
 """
-Sistema de eventos.
+Event system.
 
-Permite que los distintos módulos se comuniquen
-sin depender unos de otros.
+Allows different modules to communicate
+without depending on one another.
 """
+
+class EventBus:
+
+    def __init__(self):
+        self.listeners = {}
+
+    def on(self, event_name, callback):
+
+        if event_name not in self.listeners:
+            self.listeners[event_name] = []
+
+        self.listeners[event_name].append(callback)
+
+    def emit(self, event_name, data=None):
+
+        if event_name not in self.listeners:
+            return
+
+        for callback in self.listeners[event_name]:
+            callback(data)
